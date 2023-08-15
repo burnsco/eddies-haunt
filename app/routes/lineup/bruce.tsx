5364e1204c1a91874f52943f7694e327af6e38ce
Corey Burns <coreymburns@gmail.com>
2023-08-15 12:26:07 -0400

    Updated Deps

---
+++ app/routes/lineup/bruce.tsx
@@ -0,0 +1,33 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => [{ title: "Bruce Dickinson" }];
+
+export default function BrucePage() {
+  return (
+    <main>
+      <div className="relative min-h-screen bg-black sm:flex sm:items-center sm:justify-center">
+        <div className="relative sm:pb-16 sm:pt-8">
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 ">
+            <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl ">
+              <div className="absolute inset-0 ">
+                <img
+                  className="h-full w-full object-cover"
+                  src="https://lastfm.freetls.fastly.net/i/u/ar0/39277c12fe2c2f24a5f005aa16263d96.jpg"
+                  alt="Iron Maiden lineup"
+                />
+                <div className="absolute inset-0 hover:backdrop-blur-lg bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+              </div>
+              <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
+                <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                  <span className="animate-pulse block uppercase text-yellow-500 drop-shadow-md">
+                    Bruce Dickinson
+                  </span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
